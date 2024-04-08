284eeb09fb88ea8cfa1c7f0c3092c11a806ac8c6
Piotr Żuławnik <piotr.zulawnik14@gmail.com>
2024-04-08 19:54:43 +0200

    zad3

--- controllers/students.js
+++ controllers/students.js
@@ -1,14 +1,21 @@
-const express = require('express');
+
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('Home', { title: 'Add new student' });
+let students = [];
+let nextId = 1;
+
+router.get("/", (req, res) => {
+  res.render("Home", { title: "Add new student" });
 });
 
-router.post('/add-student', (req, res) => {
+router.post("/add-student", (req, res) => {
   const { fullName, code, fieldOfStudies } = req.body;
   console.log(`New student added: ${fullName}, ${code}, ${fieldOfStudies}`);
-  res.redirect('/');
+
+  students.push({id: nextId,fullName,code,fieldOfStudies,});
+  nextId++;
+  res.redirect("/success");
 });
 
 module.exports = router;
